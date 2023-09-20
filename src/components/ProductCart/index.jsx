f985b5f4035d2630012831f893be829f7c208f32
rezwanrahimr <rezwanrahim69@gmail.com>
2023-09-20 12:30:40 +0600

    add navbar and route setup

---
+++ src/components/ProductCart/index.jsx
@@ -0,0 +1,4 @@
+const ProductCart = () => {
+  return <></>;
+};
+export default ProductCart;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home/index";
+import MainLayout from "../layout";
+import Products from "../components/Products";
 
 
 export const router = createBrowserRouter([
     {
         path:"/",
-        element:<Home></Home>
+        element:<MainLayout/>,
+        children:[
+            {
+                path:"/",
+                element:<Home></Home>
+            },
+            {
+                path:"/products",
+                element:<Products></Products>
+            }
+        ]
     }
 ])
 
